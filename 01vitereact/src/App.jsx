0b9f5f806b38737b6ccf75cdfad59fe39eb48b5e
Udayveer07 <udayveersingh2032@gmail.com>
2025-01-29 04:11:49 +0530

    learned about JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,12 @@
 import Chai from './chai'
 
 function App() {
+  const username = "chai aur code"
 
   return (
     <>
       <Chai />
-      <h2>Chai peelo guys</h2>
+      <h2>Chai peelo guys {username}</h2>
       <p>i love chai</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,42 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Udayveer Singh </h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target= '_blank' >Visit Google</a>
+)
+
+const anotherUser = "Chai aur React"
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Go to google ',
+  anotherUser
+)
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode> is only for developers and does nothing in UI.
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+  // <MyApp />
+  // anotherElement
+  // reactElement
+  // <App/>
 )
