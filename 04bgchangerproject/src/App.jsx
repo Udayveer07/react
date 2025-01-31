ba2f33fc59326547cd08be31e82f564c3eba7d86
Udayveer07 <udayveersingh2032@gmail.com>
2025-01-31 23:57:32 +0530

    background color changer project

---
+++ 04bgchangerproject/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+
+function App() {
+  const [colour, setColour]= useState("black")
+
+  return (
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: colour }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl ">
+         <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style ={{backgroundColor:"blue"}} onClick={() => setColour("blue")}>Blue</button>
+         <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"green"}} onClick={() => setColour("green")} >Green</button>
+         <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"cyan"}} onClick={() => setColour("cyan")}>Cyan</button>
+         <button type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"teal"}} onClick={() => setColour("teal")}>Teal</button>
+         <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"lime"}} onClick={() => setColour("lime")}>Lime</button>
+         <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"red"}} onClick={() => setColour("red")}>Red</button>
+         <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"pink"}} onClick={() => setColour("pink")}>Pink</button>
+         <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" tyle ={{backgroundColor:"purple"}} onClick={() => setColour("purple")}>Purple</button>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
